2fc03292619bc1929b094eda9415dbeef8434045
inlym <inlym@qq.com>
2021-01-25 15:19:49 +0800

    :art: 优化 系统信息 页

--- pages/system/info/info.js
+++ pages/system/info/info.js
@@ -1,53 +1,90 @@
 // pages/system/info/info.js
+const app = getApp()
+
 Page({
-  /**
-   * 页面的初始数据
-   */
+  /** 页面的初始数据 */
   data: {
+    /** 设备品牌 */
     brand: '',
+
+    /** 设备型号 */
     model: '',
+
+    /** 设备像素比 */
+    pixelRatio: 0,
+
+    /** 屏幕宽度，单位px */
+    screenWidth: 0,
+
+    /** 屏幕高度，单位px */
+    screenHeight: 0,
+
+    /** 可使用窗口宽度，单位px */
+    windowWidth: 0,
+
+    /** 可使用窗口高度，单位px */
+    windowHeight: 0,
+
+    /** 状态栏的高度，单位px */
+    statusBarHeight: 0,
+
+    /** 微信设置的语言 */
+    language: '',
+
+    /** 微信版本号 */
+    version: '',
+
+    /** 操作系统及版本 */
+    system: '',
+
+    /** 客户端平台 */
+    platform: '',
+
+    /** 用户字体大小 */
+    fontSizeSetting: '',
+
+    /** 客户端基础库版本 */
+    SDKVersion: '',
+
+    /** 设备性能等级 */
+    benchmarkLevel: '',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  /** 页面初始化 */
+  init() {
     const res = wx.getSystemInfoSync()
+    app.logger.debug(res)
     this.setData(res)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  /** 生命周期函数--监听页面加载 */
+  onLoad: function (options) {
+    this.init()
+  },
+
+  /** 生命周期函数--监听页面初次渲染完成 */
   onReady: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  /** 生命周期函数--监听页面显示 */
   onShow: function () {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  /** 生命周期函数--监听页面隐藏 */
   onHide: function () {},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  /** 生命周期函数--监听页面卸载 */
   onUnload: function () {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {},
+  /** 页面相关事件处理函数--监听用户下拉动作 */
+  onPullDownRefresh: function () {
+    this.init()
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 1000)
+  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+  /** 页面上拉触底事件的处理函数 */
   onReachBottom: function () {},
 
-  /**
-   * 用户点击右上角分享
-   */
+  /** * 用户点击右上角分享 */
   onShareAppMessage: function () {},
 })
