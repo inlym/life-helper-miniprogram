30dc5db3781986fe35583d90622aa36fd7e0d9f3
inlym <inlym@qq.com>
2021-01-25 18:00:45 +0800

    :tada: 新增 搜索入口条 组件

---
+++ components/search-entry/search-entry.js
@@ -0,0 +1,28 @@
+// components/search-entry/search-entry.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    /** 输入框中默认显示文案 */
+    content: {
+      type: String,
+      value: '搜索',
+    },
+
+    url: {
+      type: String,
+      value: '/pages/index/index',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
