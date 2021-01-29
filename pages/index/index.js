c0d32c7575734e34c9e847345ce521977a67e85e
inlym <inlym@qq.com>
2021-01-29 10:54:18 +0800

    :art: 新增首页转发以及分享朋友圈的文案

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,7 +20,7 @@ Page({
 
   /** 生命周期函数--监听页面加载 */
   onLoad(options) {
-    this.init()
+    // this.init()
   },
 
   /** 生命周期函数--监听页面初次渲染完成 */
@@ -53,8 +53,22 @@ Page({
   /** 页面上拉触底事件的处理函数 */
   onReachBottom() {},
 
-  /** * 用户点击右上角分享 */
-  onShareAppMessage() {},
+  /** 用户点击右上角分享 */
+  onShareAppMessage() {
+    const imageUrl = 'https://img.lh.inlym.com/share/index_share.jpeg'
+    const title = '[来自好友推荐] 好友评价：十分实用，页面也很好看，推荐给你'
+    const path = '/pages/index/index'
+    return {
+      imageUrl,
+      title,
+      path,
+    }
+  },
+
+  /** 用户点击右上角菜单“分享到朋友圈”按钮 */
+  onShareTimeline() {
+    const title = '[来自好友推荐] 好友评价：五星好评的智能生活助手'
+  },
 
   /** 页面初始化 */
   init() {
