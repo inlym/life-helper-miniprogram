0bfc7f2a49d0928aca5ca20a895aeb1413799b72
inlym <inlym@qq.com>
2021-02-03 16:31:02 +0800

    :heavy_plus_sign: 引入 iconfont 图标文件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const app = getApp()
-const echarts = require('../../components/ec-canvas/echarts.js')
 
 Page({
   /** 页面的初始数据 */
@@ -21,11 +20,6 @@ Page({
     /** 未来 15 天的天气预报的最低温度列表 */
     minTemperature: [],
 
-    /** 未来 15 天的天气预报的折线图 */
-    ec: {
-      lazyLoad: true,
-    },
-
     /** Toptips顶部错误提示组件 */
     toptip: {
       type: 'success',
@@ -40,10 +34,7 @@ Page({
   },
 
   /** 生命周期函数--监听页面初次渲染完成 */
-  onReady() {
-    // 获取15天预报的折线图组件
-    this.ecFore15Line = this.selectComponent('#fore15line')
-  },
+  onReady() {},
 
   /** 生命周期函数--监听页面显示 */
   onShow() {},
@@ -119,46 +110,5 @@ Page({
   },
 
   /** 渲染 15 天预报的折线图 */
-  render15Line(maxTemperature, minTemperature) {
-    this.ecFore15Line.init((canvas, width, height, dpr) => {
-      const chart = echarts.init(canvas, null, {
-        width: width,
-        height: height,
-        devicePixelRatio: dpr,
-      })
-
-      const options = {
-        xAxis: {
-          type: 'category',
-          show: false,
-        },
-        yAxis: {
-          // type: 'value',
-          show: false,
-        },
-        series: [
-          {
-            label: {
-              show: true,
-              position: 'top',
-            },
-            data: maxTemperature,
-            type: 'line',
-          },
-          {
-            label: {
-              show: true,
-              position: 'bottom',
-            },
-            data: minTemperature,
-            type: 'line',
-          },
-        ],
-      }
-
-      chart.setOption(options)
-      this.chart = chart
-      return chart
-    })
-  },
+  render15Line(maxTemperature, minTemperature) {},
 })
