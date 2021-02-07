14f3284ac1e24e25aaa362518cf6be89efdec72b
inlym <inlym@qq.com>
2021-02-07 10:28:04 +0800

    :sparkles: 新增测试页（test）用于日常测试使用

---
+++ pages/test/test.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const app = getApp()
+
+Page({
+  /** 页面的初始数据 */
+  data: {},
+
+  /** 生命周期函数--监听页面加载 */
+  onLoad(options) {
+    this.init()
+  },
+
+  /** 生命周期函数--监听页面初次渲染完成 */
+  onReady() {},
+
+  /** 生命周期函数--监听页面显示 */
+  onShow() {},
+
+  /** 生命周期函数--监听页面隐藏 */
+  onHide() {},
+
+  /** 生命周期函数--监听页面卸载 */
+  onUnload() {},
+
+  /** 页面相关事件处理函数--监听用户下拉动作 */
+  onPullDownRefresh() {
+    this.init()
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+      this.setData({
+        toptip: {
+          type: 'success',
+          show: true,
+          msg: '哇哦！已经更新了哦 ~',
+        },
+      })
+    }, 1000)
+  },
+
+  /** 页面上拉触底事件的处理函数 */
+  onReachBottom() {},
+
+  /** 用户点击右上角分享 */
+  onShareAppMessage() {
+    const imageUrl = 'https://img.lh.inlym.com/share/index_share.jpeg'
+    const title = '[来自好友推荐] 好友评价：十分实用，页面也很好看，推荐给你'
+    const path = '/pages/index/index'
+    return {
+      imageUrl,
+      title,
+      path,
+    }
+  },
+
+  /** 用户点击右上角菜单“分享到朋友圈”按钮 */
+  onShareTimeline() {
+    const title = '[来自好友推荐] 好友评价：五星好评的智能生活助手'
+    return {
+      title,
+    }
+  },
+
+  /** 页面初始化 */
+  init() {},
+})
