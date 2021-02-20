b6f7d4a37e24d5b8298c830a299032cfee1cccb0
inlym <inlym@qq.com>
2021-02-20 11:08:58 +0800

    :wrench: 修改配置文件

--- app/config/config.prod.js
+++ app/config/config.prod.js
@@ -4,6 +4,9 @@ module.exports = {
   /** 服务端域名 */
   baseURL: 'https://api.lh.inlym.com',
 
-  /** 腾讯位置服务地图选点插件密钥 */
-  locationPickerKey: 'NGYBZ-KO4KD-BR34U-PATGZ-LN77S-UIFOF',
+  /** 腾讯位置服务地图选点插件 */
+  locationPicker: {
+    key: 'NGYBZ-KO4KD-BR34U-PATGZ-LN77S-UIFOF',
+    referer: '我的个人助手',
+  },
 }
