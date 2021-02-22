87d9b69839a2ba12da9b5358c80b539c334ecb4e
inlym <inlym@qq.com>
2021-02-22 13:43:44 +0800

    :monocle_face: 强化封装的 storage 方法，增加记录键保存时间

--- app/core/storage.js
+++ app/core/storage.js
@@ -3,27 +3,35 @@
 /**
  * 对原生 Storage 相关操作的简单封装，然后挂载到 app 实例上
  * @since 2021-02-05
- * @update 2021-02-20
+ * @update 2021-02-22
  */
+
+/** 用于记录保存时间的键名前缀 */
+const TIME_PREFIX = '__time__/'
 
 module.exports = {
   set(key, value) {
     wx.setStorageSync(key, value)
+    wx.setStorageSync(`${TIME_PREFIX}${key}`, new Date().getTime())
   },
 
-  write(key, value) {
-    wx.setStorageSync(key, value)
+  get write() {
+    return this.set
   },
 
   get(key) {
     return wx.getStorageSync(key)
   },
 
-  read(key) {
-    return wx.getStorageSync(key)
+  get read() {
+    return this.get
   },
 
   del(key) {
     wx.removeStorageSync(key)
+  },
+
+  time(key) {
+    return wx.getStorageSync(`${TIME_PREFIX}${key}`)
   },
 }
