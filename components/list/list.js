26cedb81b709b02812266c160196492830aa5a70
inlym <inlym@qq.com>
2021-02-26 15:41:37 +0800

    :sparkles: 新增自定义组件 列表（list）

---
+++ components/list/list.js
@@ -0,0 +1,28 @@
+// components/list/list.js
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  properties: {
+    title: {
+      type: String,
+      value: '',
+    },
+
+    list: {
+      type: Array,
+      value: [],
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
