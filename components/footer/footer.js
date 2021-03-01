e583789ebea9083e436ff545ab424acbab707f9a
inlym <inlym@qq.com>
2021-03-01 17:58:24 +0800

    :sparkles: 新增 footer 自定义组件

---
+++ components/footer/footer.js
@@ -0,0 +1,33 @@
+'use strict'
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  properties: {
+    fixed: {
+      type: Boolean,
+      value: true,
+    },
+
+    title: {
+      type: String,
+      value: '我的个人助手',
+    },
+
+    url: {
+      type: String,
+      value: '/pages/index/index',
+    },
+
+    desc: {
+      type: String,
+      value: '让你的生活更简单一点',
+    },
+  },
+
+  data: {},
+
+  methods: {},
+})
