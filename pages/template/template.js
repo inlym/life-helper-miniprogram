4824f8a33407758595290469ad7197b360f0da43
inlym <inlym@qq.com>
2021-03-23 12:24:13 +0800

    :art: 修改基本模板

--- pages/template/template.js
+++ pages/template/template.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const app = getApp()
-const { CustomPage } = app
+const { CustomPage } = getApp()
 
 CustomPage({
   /** 页面的初始数据 */
@@ -12,7 +11,7 @@ CustomPage({
   requested: {},
 
   /** 生命周期函数--监听页面加载 */
-  onLoad(options) {},
+  onLoad() {},
 
   /** 生命周期函数--监听页面初次渲染完成 */
   onReady() {},
