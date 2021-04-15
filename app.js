082522f67500ae607d0e546d5d0ef45655af134f
inlym <inlym@qq.com>
2021-04-15 10:06:12 +0800

    :wrench: 部分代码调整

--- app.js
+++ app.js
@@ -5,9 +5,4 @@ const { CustomApp } = require('./app/apps.js')
 CustomApp({
   /** 生命周期回调 —— 监听小程序初始化 */
   onLaunch() {},
-
-  /** 小程序发生脚本错误或 API 调用报错时触发 */
-  onError(error) {
-    this.logger.error(error)
-  },
 })
