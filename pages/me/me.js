38e8c95b9fd55a79c546c2ff5bc9c4ba4d8363bb
inlym <inlym@qq.com>
2021-06-20 22:13:53 +0800

    :zap: 优化了个人中心页

--- pages/me/me.js
+++ pages/me/me.js
@@ -4,22 +4,7 @@ const { CustomPage } = getApp()
 const { updateUserInfo } = require('../../app/services/userinfo')
 
 CustomPage({
-  data: {
-    list1: [
-      {
-        name: '我的设备',
-        url: '/pages/system/info/info',
-        icon: 'https://img.lh.inlym.com/icon/keyboard.png',
-      },
-    ],
-    list2: [
-      {
-        name: '关于',
-        url: '/pages/about/about',
-        icon: 'https://img.lh.inlym.com/icon/keyboard.png',
-      },
-    ],
-  },
+  data: {},
 
   requested: {
     userInfo: {
@@ -29,30 +14,6 @@ CustomPage({
 
   /** 生命周期函数--监听页面加载 */
   onLoad() {},
-
-  /** 生命周期函数--监听页面初次渲染完成 */
-  onReady() {},
-
-  /** 生命周期函数--监听页面显示 */
-  onShow() {},
-
-  /** 生命周期函数--监听页面隐藏 */
-  onHide() {},
-
-  /** 生命周期函数--监听页面卸载 */
-  onUnload() {},
-
-  /** 页面相关事件处理函数--监听用户下拉动作 */
-  onPullDownRefresh() {},
-
-  /** 页面上拉触底事件的处理函数 */
-  onReachBottom() {},
-
-  /** * 用户点击右上角分享 */
-  onShareAppMessage() {},
-
-  /** 页面初始化 */
-  init() {},
 
   onUpdateButtonTap() {
     updateUserInfo()
