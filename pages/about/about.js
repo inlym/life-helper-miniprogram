94d1ee88412a3744221730c43dc975b15158ed2c
inlym <inlym@qq.com>
2021-07-06 18:15:24 +0800

    :sparkles: 新加调试页面

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,5 +5,25 @@ const { CustomPage, getResUrl } = getApp()
 CustomPage({
   data: {
     logoUrl: getResUrl('logo'),
+
+    count: 0,
+  },
+
+  handleTap() {
+    const count = this.data.count
+    this.setData({
+      count: count + 1,
+    })
+
+    if (this.data.count >= 10) {
+      wx.showModal({
+        content: '是否进入调试模式？',
+        success(res) {
+          if (res.confirm) {
+            wx.navigateTo({ url: '/pages/debug/debug' })
+          }
+        },
+      })
+    }
   },
 })
