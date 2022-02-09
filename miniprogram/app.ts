64b7b200f31a9b25b5f86d0c3c19057786a3ca1e
inlym <inlym@qq.com>
2022-02-09 23:05:33 +0800

    :building_construction: 优化项目架构

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,18 +1,8 @@
 // app.ts
-App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+import {logger} from "./app/core/logger"
 
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
+App({
+  onLaunch() {
+    logger.debug("项目启动 ...")
   },
 })
