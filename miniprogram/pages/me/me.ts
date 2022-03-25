1097573898ca742cf75f94b5c675031066f72ffc
inlym <inlym@qq.com>
2022-03-25 20:34:37 +0800

    :sparkles: 完成个人中心页资料部分

--- miniprogram/pages/me/me.ts
+++ miniprogram/pages/me/me.ts
@@ -1,66 +1,55 @@
 // pages/me/me.ts
-Page({
+import { getUserInfo, updateUserInfo, UserInfo } from '../../app/services/userinfo'
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    /** 用户资料 */
+    userInfo: { nickName: '', avatarUrl: '' } as UserInfo,
 
+    /** LOGO 图片的本地路径（当头像为空时，使用 LOGO 替代） */
+    logoUrl: '/assets/images/common/logo.jpg',
+  },
+
+  /**
+   * 页面初始化方法
+   */
+  async init() {
+    const userInfo = await getUserInfo()
+    this.setData({ userInfo })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+    this.init()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.init()
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * 点击「更新」按钮
    */
-  onReachBottom() {
+  async onUpdateButtonTap() {
+    const result = await wx.getUserProfile({
+      lang: 'zh_CN',
+      desc: '获取您的头像和昵称',
+    })
 
+    const userInfo: UserInfo = {
+      nickName: result.userInfo.nickName,
+      avatarUrl: result.userInfo.avatarUrl,
+    }
+
+    this.setData({ userInfo })
+    await updateUserInfo(userInfo)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
