9da5c560ef50b607cfdd86cd22f5d61228c73657
inlym <inlym@qq.com>
2022-04-08 01:05:20 +0800

    :fire: 测试页初始化

--- miniprogram/pages/test/test.ts
+++ miniprogram/pages/test/test.ts
@@ -1,38 +1,15 @@
 // pages/test/test.ts
 
-import {createCanvasContext} from '../../app/utils/canvas'
-
 Page({
   /**
    * 页面的初始数据
    */
-  data: {
-    name: 'mark',
-    status: 2,
-
-    buttons: [
-      { type: 'warn', text: '按钮' },
-      { type: 'default', text: '按钮' },
-    ],
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
     //
-  },
-
-  async onReady() {
-    const ctx = await createCanvasContext('#my-canvas')
-    ctx.moveTo(100, 100)
-    ctx.quadraticCurveTo(200, 200, 300, 100)
-    ctx.stroke()
-    ctx.closePath()
-    ctx.moveTo(300, 100)
-    ctx.quadraticCurveTo(400, 200, 500, 100)
-    ctx.stroke()
-    ctx.closePath()
-
   },
 })
