7f0d137d8800e242c0493ba2458c244525e5bcfe
inlym <inlym@qq.com>
2022-04-11 22:16:48 +0800

    :building_construction: 引入暗黑模式

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,8 +1,14 @@
 // app.ts
 
-import { logger } from './app/core/logger'
+import {logger} from './app/core/logger'
 
 App({
+  /** 小程序全局数据 */
+  globalData: {
+    /** 主题，值为 light 或 dark */
+    theme: 'light',
+  },
+
   onLaunch() {
     // logger.debug('项目启动 ...')
   },
@@ -10,5 +16,10 @@ App({
   // 监听未处理的 Promise 拒绝事件
   onUnhandledRejection(res) {
     logger.debug(`[UnhandledRejection] ${res.reason}`)
+  },
+
+  /** 监听系统切换主题 */
+  onThemeChange(res) {
+    this.globalData.theme = res.theme
   },
 })
