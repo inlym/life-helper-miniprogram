89ba483c8b20031414a91b51ea08a9249805c60d
inlym <inlym@qq.com>
2022-05-03 00:43:36 +0800

    :bug: 修复主题切换跟踪器失效的问题

--- miniprogram/behaviors/theme-behavior.ts
+++ miniprogram/behaviors/theme-behavior.ts
@@ -1,5 +1,11 @@
-import {logger} from '../app/core/logger'
-
+/**
+ * 主题切换跟踪器
+ *
+ * ## 备注
+ * 1. 按照正常流程，应该在 `detached` 生命周期时进行取消主题切换监听（`wx.offThemeChange`），如果不取消监听会导致监听器不断增多。
+ * 2. 加上取消监听代码后，会导致主题切换监听异常，暂无解决办法。
+ * 3. 综上考虑，不取消监听。
+ */
 export const themeBehavior = Behavior({
   data: {
     /** 主题：'light' | 'dark' */
@@ -10,8 +16,11 @@ export const themeBehavior = Behavior({
     attached() {
       wx.onThemeChange((res) => {
         this.setData({theme: res.theme})
-        logger.debug('系统主题切换为：', res.theme)
       })
+    },
+
+    ready() {
+      this.setData({theme: wx.getSystemInfoSync().theme})
     },
   },
 })
