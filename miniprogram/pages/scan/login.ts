af016e340c863b5cc00611afdf6dc6f0e7209602
inlym <inlym@qq.com>
2022-08-09 02:35:23 +0800

    :wheelchair: 优化扫码登录页

--- miniprogram/pages/scan/login.ts
+++ miniprogram/pages/scan/login.ts
@@ -36,6 +36,23 @@ Page({
     this.scan()
   },
 
+  /** 跳转到首页 */
+  goToIndex() {
+    // 实际上可以直接跳转首页的，但是为了让用户知道发生了什么，加了这个等待提示
+    wx.showToast({
+      title: '正在跳转首页',
+      icon: 'loading',
+      mask: true,
+      duration: 2000,
+    })
+
+    setTimeout(() => {
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    }, 1500)
+  },
+
   /** 进行“扫码”操作 */
   scan() {
     const id = this.data.id
@@ -45,7 +62,22 @@ Page({
   /** 进行“确认登录”操作 */
   confirm() {
     const id = this.data.id
+    this.setData({confirmLoading: true})
     confirmQrCode(id).then(this.handleOperatorResult)
+  },
+
+  /** 点击“取消登录”按钮 */
+  cancel() {
+    wx.showToast({
+      title: '已取消登录',
+      icon: 'none',
+      duration: 2000,
+      mask: true,
+    }).then(() => {
+      setTimeout(() => {
+        this.goToIndex()
+      }, 1000)
+    })
   },
 
   /**
@@ -58,27 +90,26 @@ Page({
         region: result.region,
       })
     } else if (result.status === ScanLoginStatus.CONFIRMED) {
-      this.setData({
-        confirmLoading: false,
-      })
+      this.setData({confirmLoading: false})
 
       wx.showToast({
         title: '登录成功',
         icon: 'success',
+        mask: true,
       })
 
-      wx.switchTab({
-        url: '/pages/index/index',
-      })
+      setTimeout(() => {
+        this.goToIndex()
+      }, 1500)
     } else if (result.status === ScanLoginStatus.INVALID) {
       wx.showToast({
         title: '该二维码已失效，请重新扫码！',
         icon: 'none',
       })
 
-      wx.switchTab({
-        url: '/pages/index/index',
-      })
+      setTimeout(() => {
+        this.goToIndex()
+      }, 1500)
     } else {
       throw new Error('未知的状态！')
     }
