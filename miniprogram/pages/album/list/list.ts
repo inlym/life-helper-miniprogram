0a41a5bfad09bc5af8faad30e01112516dfd3c17
inlym <inlym@qq.com>
2022-08-13 15:46:41 +0800

    :tada: 开始写相册模块

---
+++ miniprogram/pages/album/list/list.ts
@@ -0,0 +1,18 @@
+// pages/album/list/list.ts
+// 相册模块主页：相册列表页
+
+import {shareAppBehavior} from '../../../behaviors/share-app-behavior'
+import {themeBehavior} from '../../../behaviors/theme-behavior'
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {},
+
+  behaviors: [themeBehavior, shareAppBehavior],
+
+  onLoad() {
+    // ...
+  },
+})
