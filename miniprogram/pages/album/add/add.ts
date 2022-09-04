71b2bc0c6df056006f1d37f4136fe401ff69581f
inlym <inlym@qq.com>
2022-09-05 01:12:29 +0800

    :tada: 新建「新增相册页」

---
+++ miniprogram/pages/album/add/add.ts
@@ -0,0 +1,9 @@
+import {shareAppBehavior} from '../../../behaviors/share-app-behavior'
+import {themeBehavior} from '../../../behaviors/theme-behavior'
+
+// pages/album/add/add.ts
+Page({
+  data: {},
+
+  behaviors: [themeBehavior, shareAppBehavior],
+})
