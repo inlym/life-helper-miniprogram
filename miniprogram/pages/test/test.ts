b09aeb8d4eca57875febb511a613eb56d9189d3c
inlym <inlym@qq.com>
2022-12-04 16:28:44 +0800

    :art: 将「测试页」作为页面模板

--- miniprogram/pages/test/test.ts
+++ miniprogram/pages/test/test.ts
@@ -1,27 +1,16 @@
-// pages/test/test.ts
-
-import {requestForData} from '../../app/core/http'
 import {themeBehavior} from '../../behaviors/theme-behavior'
 
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    imageUrl: '',
-  },
+  data: {},
 
   behaviors: [themeBehavior],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  /** 页面初始化方法 */
+  async init() {
+    // ...
+  },
+
   onLoad() {
-    requestForData({
-      method: 'GET',
-      url: '/login/qrcode',
-    }).then((res) => {
-      this.setData({imageUrl: res.url})
-    })
+    this.init()
   },
 })
